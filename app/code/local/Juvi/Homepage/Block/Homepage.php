1f69c3309cd98ed13bec84c4dc743fb89f5be063
Samuel Mahoney <info@divinemonkey.co.uk>
2015-04-07 23:08:19 +0100

    Added homepage

--- app/code/local/Juvi/Homepage/Block/Homepage.php
+++ app/code/local/Juvi/Homepage/Block/Homepage.php
@@ -10,15 +10,5 @@ class Juvi_Homepage_Block_Homepage extends Mage_Core_Block_Template {
 	public function getValue($value) {
 		return Mage::getStoreConfig('homepage/settings/'.$value);
 	}
-	
-	public function getCarouselArray() {
-		$carousel = array(
-			$this->getValue('image_1') => $this->getValue('text_1'),
-			$this->getValue('image_2') => $this->getValue('text_2'),
-			$this->getValue('image_3') => $this->getValue('text_3'),
-			$this->getValue('image_4') => $this->getValue('text_4')
-		);
-		return $carousel;
-	}
 
 }
